2c195cbf5dd03e705c0fba94ae42c525e3fb15ea
Vesa-Pekka Palmu <vpalmu@depili.fi>
2017-03-17 19:51:57 +0200

    Partial protocol decode!

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 )
 
 var Options struct {
-	Serial1 string `long:"serial1" description:"Serial1" default:"/dev/ttyUSB0"`
-	Serial2 string `long:"serial2" description:"Serial2" default:"/dev/ttyUSB0"`
-	Serial3 string `long:"serial3" description:"Serial2" default:"/dev/ttyUSB0"`
+	Serial1 string `long:"serial1" description:"T1" default:"/dev/ttyUSB0"`
+	Serial2 string `long:"serial2" description:"T2" default:"/dev/ttyUSB0"`
+	Serial3 string `long:"serial3" description:"R1" default:"/dev/ttyUSB0"`
+	Serial4 string `long:"serial4" description:"R2" default:"/dev/ttyUSB0"`
 }
 
 var parser = flags.NewParser(&Options, flags.Default)
@@ -24,10 +25,12 @@ func main() {
 	s1Color := color.FgYellow
 	s2Color := color.FgRed
 	s3Color := color.FgBlue
+	s4Color := color.FgGreen
 
-	go serialListen(Options.Serial1, "S1", s1Color)
-	go serialListen(Options.Serial2, "S2", s2Color)
-	go serialListen(Options.Serial3, "S3", s3Color)
+	go serialListen(Options.Serial1, "T1", s1Color)
+	go serialListen(Options.Serial2, "T2", s2Color)
+	go serialListen(Options.Serial3, "R1", s3Color)
+	go serialListen(Options.Serial4, "R2", s4Color)
 
 	for {
 		// wait
@@ -43,7 +46,6 @@ func serialListen(port, name string, c color.Attribute) {
 		Size:        8,
 		StopBits:    1,
 		ReadTimeout: time.Millisecond * 10,
-		// ReadTimeout:    options.SerialTimeout,
 	}
 
 	serial, err := serial.OpenPort(&serialConfig)
@@ -52,18 +54,121 @@ func serialListen(port, name string, c color.Attribute) {
 	}
 
 	buf := make([]byte, 256)
+	msg := make([]byte, 4)
+	msg_field := 0
+
 	for {
 		n, err := serial.Read(buf)
 		if err == nil {
 			color.Set(c)
-			sb := fmt.Sprintf("%s: ", name)
 			for i := 0; i < n; i++ {
-				sb = sb + fmt.Sprintf("%02x ", buf[i])
+				msg[msg_field] = buf[i]
+				msg_field = (msg_field + 1) % 4
+				if msg_field == 0 {
+					switch msg[0] {
+					case 0x84:
+						// Analog stuff?
+						switch msg[1] {
+						case 0x01:
+							analog(name, "Hue", msg)
+						case 0x3:
+							analog(name, "Gain", msg)
+						case 0x08:
+							analog(name, "Clip", msg)
+						case 0x4D:
+							// T-bar
+							analog(name, "T-bar", msg)
+						default:
+							analog(name, fmt.Sprintf("UNKNOWN %02x", msg[1]), msg)
+						}
+					case 0x86:
+						// Crosspoint stuff
+						switch msg[1] {
+						case 0x01, 0x02:
+							var row string
+							if msg[1] == 0x01 {
+								row = "PGM"
+							} else {
+								row = "PST"
+							}
+							switch msg[2] {
+							case 0x80:
+								button(name, fmt.Sprintf("%s Black", row))
+							case 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88:
+								button(name, fmt.Sprintf("%s %d", row, msg[2]-0x80))
+							case 0x89:
+								button(name, fmt.Sprintf("%s BKGD", row))
+							}
+						case 0x28:
+							if msg[2] == 0 && msg[3] == 0 {
+								fmt.Printf("%s: PGM button ACK\n", name)
+							} else if msg[2] == 0x01 && msg[3] == 0 {
+								fmt.Printf("%s: PST button ACK\n", name)
+							} else {
+								unknown_msg(name, msg)
+							}
+
+						case 0x64, 0x65:
+							// Upper row = 0x64, lower = 0x65
+							var row string
+							if msg[1] == 0x64 {
+								row = "PGM"
+							} else {
+								row = "PST"
+							}
+							switch msg[2] {
+							case 0x80:
+								lamp(name, fmt.Sprintf("%s Black", row), msg)
+							case 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88:
+								lamp(name, fmt.Sprintf("%s %d", row, msg[2]-0x80), msg)
+							case 0x89:
+								lamp(name, fmt.Sprintf("%s BKGD", row), msg)
+							default:
+								fmt.Printf("%s: Crosspoint lamp: %02x %02x %02x\n", name, msg[1], msg[2], msg[3])
+							}
+						default:
+							fmt.Printf("%s: Crosspoint button: %02x %02x %02x\n", name, msg[1], msg[2], msg[3])
+						}
+					case 0xE0:
+						if msg[1] == 00 && msg[2] == 00 {
+							switch msg[3] {
+							case 0x00:
+								fmt.Printf("%s: Ping1?\n", name)
+							case 0xf0:
+								fmt.Printf("%s: Ping2?\n", name)
+							default:
+								unknown_msg(name, msg)
+							}
+						} else if msg[1] == 0 && msg[2] == 0x02 && msg[3] == 0xF0 {
+							fmt.Printf("%s: Ping3?\n", name)
+						} else {
+							unknown_msg(name, msg)
+						}
+					default:
+						unknown_msg(name, msg)
+					}
+				}
 			}
-			fmt.Println(sb)
 			color.Unset()
 		}
 	}
+}
+
+func unknown_msg(name string, msg []byte) {
+	fmt.Printf("%s: %02x %02x %02x %02x\n", name, msg[0], msg[1], msg[2], msg[3])
+}
+
+func analog(name, control string, msg []byte) {
+	value := int(msg[3])*256 + int(msg[2])
+	fmt.Printf("%s: Analog %s value: %04d\n", name, control, value)
+}
+
+func button(name, button string) {
+	fmt.Printf("%s: Button %s\n", name, button)
+}
+
+func lamp(name, lamp string, msg []byte) {
+	fmt.Printf("%s: Lamp %s %02x %02x\n", name, lamp, msg[2], msg[3])
 }
 
 func decode_command(b byte) {
