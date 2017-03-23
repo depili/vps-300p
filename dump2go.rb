46a759235e8867946e796cae120e448cc590398f
Vesa-Pekka Palmu <vpalmu@depili.fi>
2017-03-23 21:17:21 +0200

    Better code

--- dump2go.rb
+++ dump2go.rb
@@ -2,5 +2,5 @@
 
 ARGF.each do |line|
   parts = line.split(" ")
-  puts "{0x#{parts[2]}, 0x#{parts[3]}, 0x#{parts[4]}, 0x#{parts[5]} }, //#{parts[6..-1].join(" ")}"
+  puts "{0x#{parts[2..5].join(", 0x")} }, //#{parts[6..-1].join(" ")}"
 end
