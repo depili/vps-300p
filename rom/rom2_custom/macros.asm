1a33cbcc03bb65f8fe6932cad7e8fe763914804c
Vesa-Pekka Palmu <vpalmu@depili.fi>
2018-12-12 19:11:02 +0200

    Formatting

--- rom/rom2_custom/macros.asm
+++ rom/rom2_custom/macros.asm
@@ -1,16 +1,16 @@
 ; Macro to zero fill memory
 ZFILL(ptr,len) MACRO
-	LD  HL,ptr
-	LD  DE,ptr+1
-	LD  BC,len-1
-	LD  (HL),0
+	LD  HL, ptr
+	LD  DE, ptr+1
+	LD  BC, len-1
+	LD  (HL), 0
 	LDIR
 ENDM
 
 ; Memory copy
 MCOPY(src,dest,len) MACRO
-	LD HL,src
-	LD DE,dest
+	LD HL, src
+	LD DE, dest
 	LD BC, len
 	LDIR
 ENDM
